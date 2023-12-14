d6a583897bf3e256bfc29b6a8bbdc9b06f3457f9
CynthiaJohn11 <ugochijohn90@gmail.com>
2023-12-14 13:02:20 -0800

    updated

--- monty.h
+++ monty.h
@@ -2,13 +2,15 @@
 #define MONTY_H
 
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
 #include <ctype.h>
 #include <sys/types.h>
-#include <fcntl.h>
-#include <stdarg.h>
+#include <unistd.h>
+
+
+#define INITIAL_BUFFER_SIZE 1024
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -17,7 +19,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
@@ -32,7 +34,7 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
@@ -40,19 +42,56 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void f_module(stack_t **head, unsigned int counter);
-void d_first(stack_t **head, unisgned int counter);
-void f_palling(stack_t **head, unsigned int counter);
-void f_pastr(stack_t **head, unsigned int counter);
-void f_rot(stack_t **head, __attribute__((unused)) unsigned int counter);
-void f_addition(stack_t **head, unsigned int counter);
-int execute(char *content, stack_t **stack, unsigned int counter, FILE *file);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_push(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
-void addnode(stack_t **head, int n);
-void free_stack(stack_t *head);
-void f_mul(stack_t **head, unsigned int counter);
-void f_pint(stack_t **head, unsigned int counter);
-void f_queue(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
+/**
+ * struct fvar_s - variables -args, file, line fcontent
+ * @ag: value
+ * @file: pointer to monty file
+ * @fcontent: line fcontent
+ * @fque: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct fvar_s
+{
+	char *ag;
+	FILE *file;
+	char *fcontent;
+	int fque;
+}  fvar_t;
+
+extern fvar_t fuse;
+
+int fexe(char *, stack_t **, unsigned int, FILE *);
+void free_fstack(stack_t *);
+/*char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);*/
+/*ssize_t getstdin(char **lineptr, int file);*/
+/*char  *clean_line(char *content);*/
+/*ssize_t _getline(char **lineptr, size_t *n, FILE *stream);*/
+
+/* basic operations */
+void s_push(stack_t **, unsigned int);
+void s_pall(stack_t **, unsigned int);
+void s_pint(stack_t **, unsigned int);
+void s_pop(stack_t **, unsigned int);
+void s_swap(stack_t **, unsigned int);
+
+/* maths operations */
+void s_add(stack_t **, unsigned int);
+void s_sub(stack_t **, unsigned int);
+void s_div(stack_t **, unsigned int);
+void s_mul(stack_t **, unsigned int);
+void s_mod(stack_t **, unsigned int);
+
+/* for other operations*/
+void s_nop(stack_t **, unsigned int);
+void s_pchar(stack_t **, unsigned int);
+void s_pstr(stack_t **, unsigned int);
+void s_rotl(stack_t **, unsigned int);
+void s_rotr(stack_t **, __attribute__((unused)) unsigned int);
+
+/* for nodes*/
+void faddnode(stack_t **, int);
+void s_stack(stack_t **, unsigned int);
+void s_queue(stack_t **, unsigned int);
+void faddqueue(stack_t **, int);
+
+#endif /* MONTY_H */
